58ac87699f119773b698a918e850457baec8b9db
VladimirKoshovec <koshavladmir@gmail.com>
2024-09-25 10:19:18 +0300

    start

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+const Menu = () => {
+    return (
+        <menu>
+            <ul>
+                <li>Гарячі пропозиції</li>
+                <li>Для бізнесу</li>
+                <li>Бюджетним установам</li>
+                <li>Послуги</li>
+                <li>Про нас</li>
+            </ul>
+        </menu>
+    );
+}
+
+
+export default Menu;
