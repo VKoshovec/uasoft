206851b5aecc32124475b9e778960c9dd112a0d2
VladimirKoshovec <koshavladmir@gmail.com>
2024-09-25 11:23:53 +0300

    add

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,14 +1,15 @@
 'use client';
+import scss from '../menu/menu.module.scss'
 
 const Menu = () => {
     return (
-        <menu>
-            <ul>
-                <li>Гарячі пропозиції</li>
-                <li>Для бізнесу</li>
-                <li>Бюджетним установам</li>
-                <li>Послуги</li>
-                <li>Про нас</li>
+        <menu className='flex mr-auto ml-auto'>
+            <ul className={scss.menu}>
+                <li><a href=''>Гарячі пропозиції</a></li>
+                <li><a href=''>Для бізнесу</a></li>
+                <li><a href=''>Бюджетним установам</a></li>
+                <li><a href=''>Послуги</a></li>
+                <li><a href=''>Про нас</a></li>
             </ul>
         </menu>
     );
